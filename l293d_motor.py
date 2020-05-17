846ad8426dfdd77c82ba8109bae43c3a129a0d6e
zkd <zkd.dut@gmail.com>
2020-05-17 16:32:04 -0700

    Finish a driver for l293d motor

--- l293d_motor.py
+++ l293d_motor.py
@@ -13,13 +13,11 @@ MO_EN = 5
 MO_1A = 6
 MO_2A = 13
 
-def motor_configure(en, 1a, 2a):
-  MO_EN = en
-  MO_1A = 1a
-  MO_2a = 2a
-  GPIO.setwarnings(False)
+def motor_configure(pin_en, pin_1a, pin_2a):
+  MO_EN = pin_en
+  MO_1A = pin_1a
+  MO_2a = pin_2a
   GPIO.setmode(GPIO.BCM)
-  button = Button(18)
   GPIO.setup(MO_EN, GPIO.OUT)
   GPIO.setup(MO_1A, GPIO.OUT)
   GPIO.setup(MO_2A, GPIO.OUT)
@@ -35,7 +33,7 @@ def clockwise(val):
   GPIO.output(MO_EN, True)
   sleep(val)
 
-def anticlockwise():
+def anti_clockwise(val):
   GPIO.output(MO_1A, False)
   GPIO.output(MO_2A, True)
   GPIO.output(MO_EN, True)
@@ -46,17 +44,27 @@ def motor_stop():
   GPIO.output(MO_2A, False)
   GPIO.output(MO_EN, False)
 
-print("Here we go! Press CTRL+C to exit")
-motor_strength(50)
-try:
-  while True:
-    if button.is_pressed:
-      print "Motor Start"
-      clockwise(3)
-    else:
-      motor_stop()
-except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
-  GPIO.cleanup() # cleanup all GPIO
+def main():
+  print("Here we go! Press CTRL+C to exit")
+  GPIO.setwarnings(False)
+  GPIO.cleanup()
+  motor_configure(MO_EN, MO_1A, MO_2A)
+  button = Button(18)
+  clock_wise_flag = 1
+  try:
+    while True:
+      if button.is_pressed:
+        print "Motor Start"
+        if clock_wise_flag == 1:
+          clockwise(3)
+          clock_wise_flag = 0
+        else:
+          anti_clockwise(3)
+          clock_wise_flag = 1
+      else:
+        motor_stop()
+  except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    GPIO.cleanup() # cleanup all GPIO
 
 if __name__=='__main__':
   main()
